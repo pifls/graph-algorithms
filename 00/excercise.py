a8c3e1bdd50e32ae5849c2fd893b1598714f0b5b
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-03 15:38:55 +0100

    displaying adjacent vertices of vertex or vertices with highest degree

--- 00/excercise.py
+++ 00/excercise.py
@@ -10,13 +10,31 @@ if f.mode == "r":
                 edge = int(char)
                 inner_matrix.append(edge)
         matrix.append(inner_matrix)
-        
+
 # displaying degree of vertices 
-index  = 0
+index = 0
+mostEdges = 0
 for vertex in matrix:
     degree = 0
     for edge in vertex:
         if edge == 1:
             degree += 1
+        if mostEdges < degree:
+            mostEdges = degree
     print('Degree of vertex ' + str(index) + ': ' + str(degree))
+    index += 1;
+
+# displaying adjacent vertices of vertex or vertices with highest degree
+index = 0
+for vertex in matrix:
+    adjacenVertives = []
+    adjacentVertex = 0
+    degree = 0
+    for edge in vertex:
+        if edge == 1:
+            degree += 1
+            adjacenVertives.append(adjacentVertex)
+        adjacentVertex += 1
+    if degree == mostEdges:
+        print('Adjacent vertices of vertex ' + str(index) + ': ' + str(adjacenVertives))
     index += 1;

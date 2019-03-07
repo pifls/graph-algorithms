b82becc9ca83471be15ea2857ca19a347e5a130d
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-07 19:25:55 +0100

    2ex added

--- 02/script.py
+++ 02/script.py
@@ -10,3 +10,53 @@ if f.mode == "r":
                 edge = int(char)
                 inner_matrix.append(edge)
         matrix.append(inner_matrix)
+graph = {}
+index = 0
+for vertex in matrix:
+    adjacenVertices = []
+    adjacentVertex = 0
+    degree = 0
+    for edge in vertex:
+        if edge == 1:
+            degree += 1
+            adjacenVertices.append(adjacentVertex)
+        adjacentVertex += 1
+    v = {index: set(adjacenVertices)}
+    graph.update(v)
+    index += 1;
+
+def dfs(graph, start):
+    path = []
+    component = []
+    vertices = []
+    for v in graph:
+        vertices.append(v)
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            component.append(vertex)
+            visited.add(vertex)
+            vertices.remove(vertex)
+            stack.extend(graph[vertex] - visited)
+        if not stack and vertices:
+            path.append(component)
+            component = []
+            stack.append(vertices[0])
+        if not stack and not vertices:
+            path.append(component)
+    return path
+
+graphComponents = dfs(graph, 0)
+
+nextVertices = []
+for component in graphComponents:
+    for vertex in component:
+        nextVertices.append(vertex)
+print("Wierzchołki w kolejności ich rozpatrzenia:")
+print(nextVertices)
+print("Liczba składowych spójności: " + str(len(graphComponents)))
+print("Kolejne składowe spójności:")
+for component in graphComponents:
+    print(component)
